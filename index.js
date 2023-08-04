d1a6af61cee7e4774f01e37a7ee443f584d4240d
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-08-04 20:51:30 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(array, driver) {
+  const newArray = array.filter((element) => {
+    if (element === driver) {
+      return element;
+    } else if (element.toLowerCase() === driver.toLowerCase()) {
+      return element;
+    }
+  });
+  return newArray;
+}
+
+function fuzzyMatch(array, letters) {
+  const newArray = array.filter((element) => element.startsWith(letters));
+  return newArray;
+}
+
+function matchName(array, driver) {
+  const newArray = array.filter((element) => element.name === driver);
+  return newArray;
+}
